e697492ebbafce97bc74b782b562165ed45f1066
Bart Joy <bart@bart.org.nz>
2014-01-16 07:51:24 +1300

    Made UploadPackage script more robust.

--- UploadPackages.ps1
+++ UploadPackages.ps1
@@ -1,14 +1,40 @@
 $ErrorActionPreference = 'Stop'
 
-function PackageViFm() {
-    cpack vifm\vifm.nuspec
-    cpush (ls vifm*.nupkg)
+function CreateAllPackages() {
+    ls -r -fi *.nuspec | % {
+        cpack $_.FullName
+        if(!$?) {
+            throw "Failed to create package."
+        }
+    }
 }
 
-pushd $PSScriptRoot
-try {
-    PackageViFm 
+function UploadAllPackages() {
+    ls *.nupkg | % {
+        cpush $_
+    }
 }
-finally {
-popd
+
+function AskForApprovalOfAllPackages() {
+    Write-Host "The following packages will be uploaded. Type YES when you have tested them to confirm they have been tested."
+    ls *.nupkg | select -Expand Name | Write-Host
+    if((Read-Host "Enter YES to upload") -ine 'YES') {
+        throw "Upload aborted because user did not confirm."
+    }
+}
+
+function RunWithinDirectory([string]$Path, [scriptblock]$Command) {
+    pushd $Path
+    try {
+        & $Command
+    }
+    finally {
+        popd
+    }
+}
+
+RunWithinDirectory $PSScriptRoot {
+    CreateAllPackages
+    AskForApprovalOfAllPackages
+    UploadAllPackages
 }
