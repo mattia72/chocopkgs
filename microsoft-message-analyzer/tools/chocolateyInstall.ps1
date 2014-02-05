d95304ecac4a1d0c18e9e95dca0c0490a06ed8da
Bart Joy <bart@bart.org.nz>
2014-02-05 22:19:59 +1300

    Added microsoft-message-analyzer package.

---
+++ microsoft-message-analyzer/tools/chocolateyInstall.ps1
@@ -0,0 +1,5 @@
+﻿$packageName = 'vifm'
+$url32 = 'http://download.microsoft.com/download/F/4/4/F4461C3D-B378-473D-B15A-28BC0BD849E8/MessageAnalyzer.msi'
+$url64 = 'http://download.microsoft.com/download/F/4/4/F4461C3D-B378-473D-B15A-28BC0BD849E8/MessageAnalyzer64.msi'
+
+Install-ChocolateyPackage 'microsoft-message-analyzer' 'msi' '/quiet' $url32 $url64
