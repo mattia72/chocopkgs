3a16f7a9e3b4e9a615ae75ef85e6cc77a3083606
Bart Joy <bart@joy.org.nz>
2016-03-16 18:39:46 +1300

    Update file hashes for new versions

--- microsoft-message-analyzer/tools/chocolateyInstall.ps1
+++ microsoft-message-analyzer/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$url32 = 'http://download.microsoft.com/download/2/8/3/283DE38A-5164-49DB-9883-9D1CC432174D/MessageAnalyzer.msi'
-$sha1Hash32 = 'F314B8C89A4974D1060F2524E7489081EE359628'
+$sha1Hash32 = '290428C4F89533D511A3D6D16A267FE7141945FB'
 $url64 = 'http://download.microsoft.com/download/2/8/3/283DE38A-5164-49DB-9883-9D1CC432174D/MessageAnalyzer64.msi'
-$sha1Hash64 = '62875FF451F13B10A8FF988F2943E76A4735D3D4'
+$sha1Hash64 = '357012E1E5D84CDE3857F5F9C27677A04EE15998'
 
 Install-ChocolateyPackage 'microsoft-message-analyzer' 'msi' '/quiet' $url32 $url64 -checksumType 'sha1' -checksum64 $sha1Hash64 -checksum $sha1Hash32
