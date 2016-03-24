3d2a1461485c6e4c7398b7a981fc3e81908172df
Bart Joy <bart@joy.org.nz>
2016-03-25 06:33:18 +1300

    Updated file hashes
    It appears that Microsoft has silently changed the installers without a
    version bump. I've updated the file hashes appropriately.

--- microsoft-message-analyzer/tools/chocolateyInstall.ps1
+++ microsoft-message-analyzer/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$url32 = 'http://download.microsoft.com/download/2/8/3/283DE38A-5164-49DB-9883-9D1CC432174D/MessageAnalyzer.msi'
-$sha1Hash32 = '290428C4F89533D511A3D6D16A267FE7141945FB'
+$sha1Hash32 = '1ECFBA636839F19D458D555337571C306047D13D'
 $url64 = 'http://download.microsoft.com/download/2/8/3/283DE38A-5164-49DB-9883-9D1CC432174D/MessageAnalyzer64.msi'
-$sha1Hash64 = '357012E1E5D84CDE3857F5F9C27677A04EE15998'
+$sha1Hash64 = '46693308FD80139C65E6DA434CEC0ECAA21909E5'
 
 Install-ChocolateyPackage 'microsoft-message-analyzer' 'msi' '/quiet' $url32 $url64 -checksumType 'sha1' -checksum64 $sha1Hash64 -checksum $sha1Hash32
