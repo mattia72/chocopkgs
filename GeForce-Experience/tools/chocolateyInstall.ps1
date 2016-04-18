18485cea46581f2997da7522c1aae07acb72d25c
mattia72 <mattia72@gmail.com>
2016-04-18 12:57:00 +0200

    delete packages not maintained by me

--- GeForce-Experience/tools/chocolateyInstall.ps1
+++
@@ -1,9 +0,0 @@
-﻿$packageName = 'geforce-experience'
-$installerType = 'EXE'
-$version = '{{PackageVersion}}'
-$url = "http://us.download.nvidia.com/GFE/GFEClient/$version/GeForce_Experience_v$version.exe"
-$silentArgs = '/s /noreboot'
-$validExitCodes = @(0)
-
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
-Write-Output "If you experience errors after updating, you may need to reboot."
