e02394b395636583007820db75fda99a9ab57492
mattia72 <mattia72@gmail.com>
2016-05-11 20:47:42 +0200

    depricate vim-x86 packages

--- vim-x86.portable/tools/chocolateyinstall.ps1
+++
@@ -1,9 +0,0 @@
-﻿$packageName = 'vim-x86.portable'
-$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$destDir = Join-Path $toolsDir "vim74"
-$url = "http://tuxproject.de/projects/vim/complete-x86.7z"
-$url86 = $url
-
-Get-ChildItem "$destDir\*.bat" | %{ Install-BinFile -Name $_.BaseName -Path $_ }
-Install-ChocolateyZipPackage $packageName $url $destDir $url86
-Start-Process "$destDir\install.exe" -ArgumentList "-add-start-menu" -WorkingDirectory "$destDir" -Wait
--- vim-x86/tools/chocolateyinstall.ps1
+++
@@ -1,1 +0,0 @@
-#Install-VirtualPackage 'vim-x86.portable' 'vim-x86.install'
